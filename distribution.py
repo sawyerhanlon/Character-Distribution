2bb2fd9ccc9e7fa202a3b88543d9733ce05b906e
sawyerhanlon <sawyerhanlon@hanovernorwichschools.org>
2015-10-15 10:08:49 -0400

    Updated from Brython Server: October 15, 2015 10:08:49 AM EDT

--- distribution.py
+++ distribution.py
@@ -36,3 +36,7 @@ Notice about this example:
   in the text and they are listed in the output in alphabetical order.
 * Letters that do not occur in the text are not listed in the output at all.
 """
+import string
+string.ascii_lowercase
+
+string = input("Please enter a string of text (the bigger the better): ")

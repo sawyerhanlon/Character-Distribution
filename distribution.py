0a35129affadb3e701b71f6dc62b4f74df86801f
sawyerhanlon <sawyerhanlon@hanovernorwichschools.org>
2015-10-20 14:11:34 -0400

    Updated from Brython Server: October 20, 2015 2:11:34 PM EDT

--- distribution.py
+++ distribution.py
@@ -36,8 +36,6 @@ Notice about this example:
   in the text and they are listed in the output in alphabetical order.
 * Letters that do not occur in the text are not listed in the output at all.
 """
-import string
-alph = string.ascii_lowercase
 alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 dis = []
 y = input("Please enter a string of text (the bigger the better): ").lower()
